c9d7a6f821d269d425658dd691275a298037c012
arun <arunartic76@gmail.com>
2026-05-24 00:29:00 +0530

    Update section_c_reports.sql

--- queries/section_c_reports.sql
+++ queries/section_c_reports.sql
@@ -1,22 +1,16 @@
 USE DiagnosticLabDB;
 GO
 
--- 8. Report status counts
-
 SELECT
     Status,
     COUNT(*) AS CountStatus
 FROM lab.Report
 GROUP BY Status;
 
--- 9. Reports dispatched vs total
-
 SELECT
     SUM(CASE WHEN Status = 'Dispatched' THEN 1 ELSE 0 END) AS Dispatched,
     COUNT(*) AS TotalReports
 FROM lab.Report;
-
--- 10. Total pending reports
 
 SELECT COUNT(*) AS PendingReports
 FROM lab.Report
