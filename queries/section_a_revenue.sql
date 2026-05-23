47eefd13ea39c7ce316ce1730f69f942e17a63c4
arun <arunartic76@gmail.com>
2026-05-24 00:27:09 +0530

    Update section_a_revenue.sql

--- queries/section_a_revenue.sql
+++ queries/section_a_revenue.sql
@@ -1,18 +1,12 @@
 DECLARE @ReportDate DATE = CAST(GETDATE()-1 AS DATE);
 
--- 1. Total bookings
-
 SELECT COUNT(*) AS TotalBookings
 FROM lab.Booking
 WHERE CAST(BookingDate AS DATE) = @ReportDate;
 
--- 2. Total revenue
-
 SELECT SUM(PaidAmount) AS TotalRevenue
 FROM lab.Payment
 WHERE CAST(PaidOn AS DATE) = @ReportDate;
-
--- 3. Revenue by payment mode
 
 SELECT
     PaymentMode,
@@ -21,14 +15,10 @@ FROM lab.Payment
 WHERE CAST(PaidOn AS DATE) = @ReportDate
 GROUP BY PaymentMode;
 
--- 4. Average booking value
-
 SELECT
     AVG(TotalAmount) AS AvgBookingValue
 FROM lab.Booking
 WHERE CAST(BookingDate AS DATE) = @ReportDate;
-
--- 5. Top 3 tests by revenue
 
 SELECT TOP 3
     t.TestName,
